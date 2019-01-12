16ec633346357f12d747e3e73cc99edc4c643be0
dmar <dan.britain.marshall@gmail.com>
2019-01-11 22:07:07 -0600

    rough draft

---
+++ assets/javascript/app.js
@@ -0,0 +1,122 @@
+// global variables
+
+var gameLength=10;
+var answerTimer=3;
+var questionLength= 10;
+var library;
+var timeToGuess;
+var timer;
+var currentQuestion;
+var numberRight;
+var numberWrong;
+
+//function to display intro
+
+function initGame() {
+	$("#qText").html('Welcome to my James Bond Trivia Game <button id="startGame">Begin Game</button>')
+	$("#result").hide();
+	$("#choices").hide();
+	$("#choices li").empty();
+	$(".scoreBoard").empty();
+	$("#choices .answer").off().on("click", makeGuess);
+	$("#startGame").off().on("click", newQuestion);
+	numberWrong=0;
+	numberRight=0;
+	library= questionLibrary.slice();
+	timeToGuess= questionLength;
+	gameLength= library.length;
+}
+
+//new question function
+
+function newQuestion (){
+	if (numberRight+numberWrong >= gameLength) {
+			gameOver ();
+	} else {
+		var questionNumber = Math.floor(Math.random()* library.length);
+		currentQuestion= library[questionNumber];
+		library.splice (questionNumber, 1);
+		resetTimer();
+		$("#result").empty().hide();
+		$("#qText").html(currentQuestion.question);
+		$("#choices").show().find(".answer").each(function(i){
+				$(this).html(currentQuestion.answers[i]);
+		});
+
+	}
+}
+
+//make guess function
+
+function makeGuess () {
+	if ($(this).data("choice") == currentQuestion.correctAnswer){
+			numberRight ++;
+			showResult("Correct!", "correctResult");
+		} else {
+			numberWrong ++;
+			showResult ("Wrong. The correct answer was " + currentQuestion.answers [currentQuestion.correctAnswer], "wrongResult");
+		}
+
+}
+
+
+//showresults function
+function showResult (msg, addThisClass) {
+	resetTimer ();
+	$("#result")
+		.html(msg)
+		.show()
+		.removeClass()
+		.addClass(addThisClass);
+	setTimeout (newQuestion, answerLength * 1000);
+	$("#score").html("correct:" + numberRight + "<br> incorrect:"+ numberWrong);
+		
+}
+
+//show timer
+function showTimer(){
+	if (timeToGuess >= 0){
+		$("#timer").html(timeToGuess + " seconds left");
+		timeToGuess --;
+	} else {
+		timesUp();
+	}
+}
+function timesUp(){
+	numberWrong ++;
+	resetTimer();
+	showResult("Time's Up! The correct answer was " + currentQuestion.answers[currentQuestion.correctAnswer], "timesUp");
+}
+function resetTimer(){
+	clearInterval(timer);
+	timeToGuess = questionLength;
+	$("#timer").empty();
+}
+function gameOver(){
+	$("body").css("background-image", 'url("../assets/images/background.png")');
+	var score = (numberRight/gameLength);
+	var praise = "That was pretty shabby. You get sent to the Wall.";
+	if (score > .9){
+		praise = "Amazing! You achieved the rank of King of the Seven Kingdoms.";
+	} else if (score > .8){
+		praise = "Amazing! You achieved the rank of Knight of the Kingsguard.";
+	} else if (score > .7){
+		praise = "Well done, you achieved the rank of Lord of Harrenhal.";
+	} else if (score > .6){
+		praise = "Well done, you achieved the rank of Maester.";
+	} else if (score > .5){
+		praise = "Well done, you achieved the rank of Knight's Squire.";
+	} else if (score > .4){
+		praise = "Not bad, you achieved the rank of Novice of the Citadel.";
+	}
+	$("#result").removeClass().html("<h1>Game Over</h1><div class='gameOverText'>You got " + numberRight + " questions right and " + numberWrong + " wrong. " + praise + "</div><button id='newGame'>Play Again</button>");
+	$("#newGame").on("click", initGame);
+}
+$(document).ready(initGame);
+
+
+//times up
+
+// reset timer
+
+//game over function
---
+++ assets/javascript/triviaQuestions.js
@@ -0,0 +1,43 @@
+var questionLibrary = [
+
+	{
+		question: "What was the name of the first Bond film?",
+		answers: ["Dr. No","Casino Royale","On Her Majesty's Secret Service"],
+		correctAnswer: 0,
+		image: ""
+	},
+	{
+		question: "Who was the first actor to play 'M'?",
+		answers: ["Bernard Lee","Robert Brown","Jack Lord"],
+		correctAnswer: 0,
+		image: ""
+	},
+	{
+		question: "Which was the first Bond film to star Pierce Brosnan?",
+		answers: ["Tomorrow Never Dies","Die Another Day","Goldeneye"],
+		correctAnswer: 2 ,
+		image: ""
+	},
+	{
+		question: "What is the title of the 23rd James Bond film?",
+		answers: ["Skyscream","Bullet to The Head","Skyfall"],
+		correctAnswer: 2 ,
+		image: ""
+	},
+	{
+		question: "How is Major Boothroyd better known?",
+		answers: ["MB","Q","Bootsy"],
+		correctAnswer: 1,
+		image: ""
+	},
+	{
+		question: "Which Bond film is set primarily in Japan?",
+		answers: ["Octopussy","You Only Live Twice","Die Another Day"],
+		correctAnswer: 1,
+		image: ""
+	}
+
+]
+
+
+
